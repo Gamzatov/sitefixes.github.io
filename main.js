52193b28e207b41d84c466a283403919e8c49a09
gamzatov <dirik@Diriks-MacBook-Pro.local>
2024-11-29 12:58:55 +0200

    smooth scroll

--- main.js
+++ main.js
@@ -37,3 +37,15 @@ class NavigationController {
 }
 
 new NavigationController();
+
+// smooth scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
