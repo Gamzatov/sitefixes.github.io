77f156af136ab1a6ef0f1f9b871df50c25e3bf0a
gamzatov <dirik@Diriks-MacBook-Pro.local>
2024-12-02 12:38:50 +0200

    adding gallery

--- main.js
+++ main.js
@@ -57,3 +57,32 @@ $(document).ready(function () {
         return false;
     });
 });
+
+// gallery 
+$(document).ready(function () {
+
+    $("#owl-gallery").owlCarousel({
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        },
+        navigation: true,
+        navigationText: ['<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span>', '<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span>'],
+    });
+
+});
